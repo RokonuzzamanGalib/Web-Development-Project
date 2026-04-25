f3b736631857ac1ae2a0a8fed8c8e59234ac5403
RokonuzzamanGalib <rokonuzzamangalib@gmail.com>
2026-04-25 12:21:40 +0600

    Frontend Work

--- ecommerce-app/frontend/src/pages/Home.jsx
+++ ecommerce-app/frontend/src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import LatestCollection from '../components/LatestCollection'
 
-const home = () => {
+
+const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Hero/>
+      <LatestCollection/>
+    </div>
   )
 }
 
-export default home
+export default Home
